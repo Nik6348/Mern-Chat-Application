b4149314cab96c3201c521733ba2a8def92a81c0
Nikhil Rajput <150193253+Nik6348@users.noreply.github.com>
2024-06-04 08:00:57 +0530

    Update Login.jsx

--- Chat-App-FE/src/components/Login.jsx
+++ Chat-App-FE/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/users/login", {
+      await axios.post("mern-chat-application-axct-mnvos3891-nik6348s-projects.vercel.app/api/users/login", {
         username,
         password,
       });
